ded565947b3d59b103b8a8860d634e707f6114bb
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-03-20 13:57:28 +0200

    redux

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/redux/auth/operations.js
@@ -0,0 +1,58 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { walletApi, setToken, removeToken } from '../../axiosConfig/walletApi';
+
+export const registerThunk = createAsyncThunk(
+  'register',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await walletApi.post('auth/sign-up', credentials);
+      setToken(data.token);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginThunk = createAsyncThunk(
+  'login',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await walletApi.post('auth/sign-in', credentials);
+      setToken(data.token);
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutThunk = createAsyncThunk('logout', async (_, thunkAPI) => {
+  try {
+    const { data } = await walletApi.delete('auth/sign-out');
+    console.log(data);
+
+    removeToken();
+
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshThunk = createAsyncThunk('refresh', async (_, thunkApi) => {
+  const savedToken = thunkApi.getState().auth.token;
+  if (!savedToken) {
+    return thunkApi.rejectWithValue('Token is not exist!');
+  }
+  try {
+    setToken(savedToken);
+    const { data } = await walletApi.get('users/current');
+    return data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.message);
+  }
+});
