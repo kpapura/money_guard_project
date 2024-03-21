006ac0892b34be81e608f9f6c3c603cd5ea94491
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-03-21 19:16:03 +0200

    currency fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,48 +40,4 @@ function App() {
 
 export default App;
 
-// import React from 'react';
-// import Login from '../pages/Login/Login';
-// import { Route, Routes } from 'react-router-dom';
-// import Layout from './Layout/Layout';
-// import Register from '../pages/Register/Register';
-// import StatisticsTab from '../pages/StatisticsTab/StatisticsTab';
-// import 'modern-normalize';
 
-// function App() {
-//   return (
-//     <div>
-//       <h1>Hello</h1>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route path="login" element={<Login />} />
-//           <Route path="register" element={<Register />} />
-//         </Route>
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { fetchCurrencyRate } from '../redux/currencyRate/operations';
-// import CurrencyRates from './CurrencyRates/CurrenceRate';
-
-// function App() {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchCurrencyRate());
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       <h1>Мій додаток</h1>
-//       <CurrencyRates />
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/CurrencyRates/CurrenceRate.jsx
+++ src/components/CurrencyRates/CurrenceRate.jsx
@@ -1,49 +1,4 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
 
-// // import image from '../img/currency_mobile.webp';
-
-// import s from './CurrenceRate.module.css'
-// import { fetchCurrencyRate } from '../../redux/currencyRate/operations';
-
-// function CurrencyRates() {
-
-//   // const currencyRate = useSelector(state => state.currency.currencyRate);
-
-//   // if (!currencyRate) {
-//   //   return <p>Loading...</p>;
-//   // }
-
-//   return (
-//     <div className={s.wrapper}>
-//       <p>qwerty</p>
-//       {/* <ul className={s.title}>
-//         <li className={s.titleItem}>Currency</li>
-//         <li className={s.titleItem}>Purchase</li>
-//         <li className={s.titleItem}>Sale</li>
-//       </ul> */}
-//       {/* {usdRate && (
-//         <ul className={s.row}>
-//           <li className={s.rowItem}>USD</li>
-//           <li className={s.rowItem}>{usdRate.rateBuy}</li>
-//           <li className={s.rowItem}>{usdRate.rateSell}</li>
-//         </ul>
-//       )}
-//       {eurRate && (
-//         <ul className={s.row}>
-//           <li className={s.rowItem}>EUR</li>
-//           <li className={s.rowItem}>{eurRate.rateBuy}</li>
-//           <li className={s.rowItem}>{eurRate.rateSell}</li>
-//         </ul> */}
-//           {/* )} */}
-//           {/* <img src={image} alt="" /> */}
-
-//       {/* <img src="../img/currency_mobile.webp" alt="картинка відсутня" /> */}
-//     </div>
-//   );
-// }
-
-// export default CurrencyRates;
 
 import React from 'react';
 import { useSelector } from 'react-redux';
