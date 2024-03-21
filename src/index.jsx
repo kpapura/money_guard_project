a6e11643c5b6b90f3dfcd0a4f179c1717f68e185
kpapura <125825500+kpapura@users.noreply.github.com>
2024-03-21 09:52:23 +0200

    baseName

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/money_guard_project'>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
