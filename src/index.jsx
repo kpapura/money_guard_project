f50b6a69dd6ebc9cedb49b79feab72bae655a240
kpapura <125825500+kpapura@users.noreply.github.com>
2024-03-21 07:57:14 +0200

    updated form for adding and editing tansactions

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,14 @@ import App from './components/App';
 
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
+        <ToastContainer autoClose={1500} />
       </PersistGate>
     </Provider>
   </React.StrictMode>
