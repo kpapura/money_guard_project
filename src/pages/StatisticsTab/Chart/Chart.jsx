1513094f9b945c028c69187bfa555690e563c264
Baromental <suzdalcevmaksym@gmail.com>
2024-03-24 12:24:47 +0200

    made chart and table logic

--- src/pages/StatisticsTab/Chart/Chart.jsx
+++ src/pages/StatisticsTab/Chart/Chart.jsx
@@ -1,27 +1,39 @@
 // Chart.jsx
 import React from 'react';
+
 import { Doughnut } from 'react-chartjs-2';
 import s from './Chart.module.css';
 import Chart from 'chart.js/auto'; 
 
-const DoughnutChart = ({ transactions, colors }) => {
+const DoughnutChart = ({ transactions, colors, period }) => {
    if (!transactions || !colors) {
-    return <div>No data available</div>; // Повертаємо повідомлення про відсутність даних
+    return <div>No data available</div>;
   }
+
+  const formattedPeriod = period.toFixed(2);
+
   const data = {
-    labels: transactions?.map(transaction => transaction.name),
     datasets: [{
       data: transactions?.map(transaction => transaction.total),
       backgroundColor: transactions?.map(transaction => colors[transaction.name]),
-      hoverBackgroundColor: transactions?.map(transaction => colors[transaction.name])
+      hoverBackgroundColor: transactions?.map(transaction => colors[transaction.name]),
+      cutout: 95
+      
     }]
   };
 
   return (
     <div className={s.chartContainer}>
-      <Doughnut data={data} />
+      <div className={s.chartWrapper}>
+        <Doughnut data={data} />
+        <p className={s.periodText}> ₴ {formattedPeriod}</p> 
+      </div>
     </div>
   );
 };
 
 export default DoughnutChart;
+
+
+
+  
