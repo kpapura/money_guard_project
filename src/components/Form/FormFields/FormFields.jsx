624beae584c18df87a124dac54c9bc9673133546
kpapura <125825500+kpapura@users.noreply.github.com>
2024-03-25 01:01:05 +0200

    finished add/edit form logic and styles

--- src/components/Form/FormFields/FormFields.jsx
+++ src/components/Form/FormFields/FormFields.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import s from "../Form.module.css"
+import s from '../Form.module.css';
 
 const FormInput = ({ name, register, errors, placeholder, className }) => {
   return (
     <div className={s.inputContainer}>
       <input
+        autoComplete="off"
         className={className}
         {...register(name)}
         placeholder={placeholder}
