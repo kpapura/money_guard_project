afcf8ebfe03b1673fe4c0e649f211ec78678811f
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-03-24 13:51:14 +0200

    Balance and Currency upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,62 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import Login from '../pages/Login/Login';
+// import { Route, Routes } from 'react-router-dom';
+// import { Layout } from './Layout';
+// import Register from '../pages/Register/Register';
+// import StatisticsTab from '../pages/StatisticsTab/StatisticsTab';
+// import 'modern-normalize';
+// import { refreshThunk } from '../redux/auth/operations';
+// import { useDispatch } from 'react-redux';
+// import HomeTab from '../pages/HomeTab/HomeTab';
+
+// function App() {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(refreshThunk());
+//   }, [dispatch]);
+
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route path="login" element={<Login />} />
+//           <Route path="register" element={<Register />} />
+//           <Route path="home" element={<HomeTab />} />
+//         </Route>
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from 'react';
 import Login from '../pages/Login/Login';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import Register from '../pages/Register/Register';
 import StatisticsTab from '../pages/StatisticsTab/StatisticsTab';
 import 'modern-normalize';
-import { refreshThunk } from '../redux/auth/operations';
-import { useDispatch } from 'react-redux';
-import HomeTab from '../pages/HomeTab/HomeTab';
+import CurrencyRates from './CurrencyRates/CurrenceRate';
+import Balance from './Balance/Balance';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(refreshThunk());
-  }, [dispatch]);
-
   return (
     <div>
-      <Routes>
+      <h1>Hello</h1>
+      <CurrencyRates />
+      <Balance/>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="home" element={<HomeTab />} />
         </Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
-
 export default App;
+
