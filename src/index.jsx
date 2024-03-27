e77f6e0aa123c8e66d8757d76e4b0a36892ad4af
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-03-27 09:17:14 +0200

    toast + fetchCurrencyRate

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import App from './components/App';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <BrowserRouter basename='/money_guard_project'>
--- src/redux/currencyRate/operations.js
+++ src/redux/currencyRate/operations.js
@@ -4,27 +4,13 @@ import { toast } from 'react-toastify';
 
 export const fetchCurrencyRate = createAsyncThunk(
   'currency/fetchCurrencyRate',
-  async () => {
-    const savedData = localStorage.getItem('currencyRate');
-    const savedTime = localStorage.getItem('lastFetchTime');
-
-    if (savedData && savedTime) {
-      const currentTime = new Date().getTime();
-      const timeDifference = (currentTime - savedTime) / (1000 * 60 * 60);
-
-      if (timeDifference < 1) {
-        return JSON.parse(savedData);
-      }
-    }
-
+  async (_, thunkApi) => {
     try {
       const response = await axios.get('https://api.monobank.ua/bank/currency');
-
-      localStorage.setItem('currencyRate', JSON.stringify(response.data));
-      localStorage.setItem('lastFetchTime', new Date().getTime().toString());
       return response.data;
     } catch (error) {
       toast.error('Помилка при виконанні запиту до Monobank:', error);
+      return thunkApi.rejectWithValue(error.message);
     }
   }
 );
